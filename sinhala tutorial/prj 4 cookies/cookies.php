3ab5108d46ba308452b9468c945d44facb6e0687
sajeewa garusinghe <sajeeva616@gmail.com>
2022-05-19 22:05:27 +0530

    fnal commit to cookies file
    his is final commit to ookies file

--- sinhala tutorial/prj 4 cookies/cookies.php
+++ sinhala tutorial/prj 4 cookies/cookies.php
@@ -4,8 +4,15 @@
 //setcookie(name,value,expiration);
 
 //setcookie('language','sinhala',time()+60*60*24*7);
+//remove cookie >>>setcookie('language','sinhala',time()-60*60*24*7);
 //cookie data saved in $_COOKIE super globl variable
-$language=$_COOKIE['language'];
+if(isset($_COOKIE['language'])){
+    $language=$_COOKIE['language'];
+    echo "selected language :{$language}";
+}else{
+    setcookie('language','sinhala',time()+60*60*24*7);
+}
+
 
 ?>
 
@@ -18,6 +25,8 @@ $language=$_COOKIE['language'];
     <title>cookies</title>
 </head>
 <body>
-    cookies
+    <h1>
+        cookies
+    </h1>
 </body>
 </html>
