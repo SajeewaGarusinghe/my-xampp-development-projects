183ce848c72ba0f9b5f69de7bffc23a114245b2f
sajeewa garusinghe <sajeeva616@gmail.com>
2022-05-21 10:20:20 +0530

    insert data to querry

--- sinhala tutorial/prj 6_mysql/index.php
+++ sinhala tutorial/prj 6_mysql/index.php
@@ -1,5 +1,26 @@
+<?php  require_once('inc/connection.php') ?>
+
 <?php
-require_once('inc/connection.php') ?>
+// -- INSERT INTO TABLE_NAME(COL1,COL2,COL3,ETC) VALUES(VAL1 ,VAL2,VAL3 ,ETC)//INERT DATA TO QUERRY MY SQL COMMAND
+//  --
+$first_name="samadhi";
+$last_name="ashed";
+$email="samaddhi@gmail.com";
+$password='mypassword12';
+$hashed_password=sha1($password);
+$is_deleted=0;
+
+$query="INSERT INTO users (first_name,last_name,email,password,is_deleted) VALUES ('{$first_name}','{$last_name}','{$email}','{$hashed_password}',{$is_deleted})";
+
+// data insert to table using following method
+$result=mysqli_query($connection,$query);
+if($result){
+echo 'one record added';
+}else{
+    echo 'update fail';
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +31,7 @@ require_once('inc/connection.php') ?>
     <title>Document</title>
 </head>
 <body>
-    <h1>home page</h1>
+    <h1>insert query</h1>
 
     <img src="img/project.png" alt=""><br>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est optio odit ea nobis sunt id amet delectus dignissimos veniam aspernatur consequatur ut nisi laboriosam, quo cupiditate, quibusdam alias harum et.</p>
@@ -19,6 +40,6 @@ require_once('inc/connection.php') ?>
 </html>
 
 <?php
-mysqli_close($connection);//to cloe the database connection made earlier
+mysqli_close($connection);//to close the database connection made earlier
 
 ?>
